802b3c05e55d006551804c4abbf71c622f0cacf3
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2023-03-05 22:53:05 +0100

    extend technology stack description

--- src/components/Terminal/rowManager.ts
+++ src/components/Terminal/rowManager.ts
@@ -139,7 +139,7 @@ const generateTextForTechnologies = (currentId: number): ITerminalRow[] => {
       active: false,
       type: 'CUSTOM_TEXT',
       value:
-        '  Technologies & Frameworks     - Node.js, React.js, Next.js, Angular, Spring, git, Docker, K8s, GraphQL, Postman, Gitlab/Github',
+        '  Technologies & Frameworks     - Node.js, React.js, Next.js, Angular, Spring, git, Docker, K8s, GraphQL, RabbitMQ, Postman, Gitlab/Github',
     },
   ];
 };
